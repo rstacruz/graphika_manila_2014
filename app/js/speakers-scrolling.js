a583450d7040b503ad4ab309b08a97ff8624c9b8
Rico Sta. Cruz <hi@ricostacruz.com>
2013-12-08 22:46:07 +0800

    Menu: speaker images in the menu.

--- app/js/speakers-scrolling.js
+++ app/js/speakers-scrolling.js
@@ -5,23 +5,33 @@ $(function () {
   var $speakers = $(".speaker.section");
 
   // Separate scrollagent for menu activation
-  $('body > [id]').scrollagent(function (cid, pid, el, previous) {
+  $('body > .section[id]').scrollagent(function (cid, pid, el, previous) {
     if (cid) $('a[href="#'+cid+'"]').addClass('active');
     if (pid) $('a[href="#'+pid+'"]').removeClass('active');
   });
 
   $('.section[id], .m-section').scrollagent({
-    xform: function (y, range, height) { return y + height * 0.8; }
+    xform: function (y, range, height) { return y + height * 0.2; }
   }, function (cid, pid, el, previous) {
-    if (cid) $('a[href="#'+cid+'"]').addClass('active');
-    if (pid) $('a[href="#'+pid+'"]').removeClass('active');
+    // Fix navigation bar
+    var isSpeaker = ($(el).closest('#speakers').length > 0);
+    $('.navigation-bar').toggleClass('show-speakers', isSpeaker);
+    $(".speaker-link a.active").removeClass('active');
+    if (isSpeaker) {
+      $('a[href="#'+cid+'"]').addClass('active');
+    }
 
+    // Highlight the current class (for whatever reason)
     $(el).addClass('active');
     $(previous).removeClass('active');
-    $(document).trigger('section', cid);
+
+    // Update root classname (transitions stuff via CSS)
     $('html')
       .toggleClass(getClass($(previous)), false)
       .toggleClass(getClass($(el)), true);
+
+    // Let others know
+    $(document).trigger('section', cid);
   });
 
   function getClass($el) {
