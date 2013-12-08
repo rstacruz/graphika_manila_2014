51a9fd6eb624594aa7b1ceda0f8f7e0e6248cd90
Rico Sta. Cruz <hi@ricostacruz.com>
2013-12-09 04:42:17 +0800

    .

--- assets/app.js
+++ assets/app.js
@@ -244,18 +244,5 @@ $(function () {
 });
 
 
-;$(function () {
-  $('video').each(function () {
-    var $video = $(this);
-    var vid = $video[0];
-
-    vid.addEventListener('ended', function () {
-      console.log("END");
-      vid.currentTime = 0.1;
-      vid.play();
-    }, false);
-  });
-});
-
 ;
 //# sourceMappingURL=app.js.map
