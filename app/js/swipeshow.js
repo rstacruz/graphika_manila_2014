f5125263e22c37a730636b503bd64b77c39bf867
Rico Sta. Cruz <hi@ricostacruz.com>
2013-12-17 00:00:59 +0800

    Remove extra images on mobile.

--- app/js/swipeshow.js
+++ app/js/swipeshow.js
@@ -1,8 +1,12 @@
 $(function() {
-  $('.swipeshow').each(function () {
-    $(this).append('<div class="dots">');
-  });
-  $('.swipeshow').swipeshow({
-    autostart: false
-  });
+  if ($('html').is('desktop')) {
+    $('.swipeshow').each(function () {
+      $(this).append('<div class="dots">');
+    });
+    $('.swipeshow').swipeshow({
+      autostart: false
+    });
+  } else {
+    $(".swipeshow .slide + .slide").remove();
+  }
 });
